8996943344a87d781fc319dc5332633f72b3ee7e
Antonio Alvarez Lopez <120938774+intentodepirata@users.noreply.github.com>
2023-07-03 01:14:07 +0200

    Update Usuario.js

--- SCRIPTS/Usuario.js
+++ SCRIPTS/Usuario.js
@@ -1,10 +1,1 @@
-function ProductoAlimenticio(codigo, nombre, precio) {
-    this.codigo = codigo;
-    this.nombre = nombre;
-    this.precio = precio;
-    this.imprimeDatos = function () {
-      console.log(this.codigo, this.nombre, `${this.precio} €`);
-    };
-  }
-
 
